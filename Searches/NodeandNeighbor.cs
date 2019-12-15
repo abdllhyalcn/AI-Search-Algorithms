a4d3dc5c3f1f40b41d0d8da20e643f2f04c929ac
abdllhyalcn <49105577+abdllhyalcn@users.noreply.github.com>
2019-12-16 02:59:32 +0300

    Share of the Interface and Structures for Search classes.
    It can be usable infrastructure for search algorithms to see in C#.

---
+++ Searches/NodeandNeighbor.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Searches
+{
+    //Summary:
+    //      Stores the neighbor and distance to the neighbor.
+    struct Neighbor<T>
+    {
+        public Node<T> neighbor { get; }
+        public int DistanceToNeighbor { get; }
+        public Neighbor(Node<T> neighbor,int Distance)
+        {
+            this.neighbor=neighbor;
+            this.DistanceToNeighbor=Distance;
+        }
+    }
+    // Summary:
+    //      Represents a node for a Graph Data Structure
+    // Variables:
+    //      Name:Represents the Node name given int the constructor.
+    //      Depth:Stores the depth attribute of the node.
+    //      Neighbors:Includes Neighbor struct.
+    //      IsVisited:Control variable that is used in searches.
+    //      HanG:Stores the heuristic and actual cost.
+    //      F:is the sum of heuristic and actual cost.
+    class Node<T>
+    {
+        public string Name { get; }
+        public int Depth;
+        public List<Neighbor<T>> Neighbors { get; }
+        public bool IsVisited { get; set; } = false;
+
+        public int[,] HandG=new int[1,2];
+        public int F => HandG[0,0]+HandG[0,1];
+
+        // Summary:
+        //      Initializes the Node Class. 
+        // Parameters:
+        //      name:The node name.
+        //      h:Heuristic value.
+        //      g:Actual cost value.
+        //      neighbors:The initialized neighbor list of the node.
+        public Node(string name,int h=0,int g=0, IEnumerable<Neighbor<T>> neighbors = null)
+        {
+            Name = name;
+            Neighbors = neighbors?.ToList() ?? new List<Neighbor<T>>();
+            this.HandG =new int[,]{ { h, g } };
+        }
+        // Summary:
+        //      Initializes the Node Class. 
+        // Parameters:
+        //      name:The node name.
+        //      neighbors:The initialized neighbor list of the node.
+        public Node(string name, IEnumerable<Neighbor<T>> neighbors = null)
+        {
+            Name = name;
+            Neighbors = neighbors?.ToList() ?? new List<Neighbor<T>>();
+        }
+        // Summary:
+        //       Adds neighbor into Neighbors list.
+        // Parameters:
+        //       neighbor: is a Neighbor struct.
+        public void AddEdge(Neighbor<T> neighbor)
+        {
+            Neighbors.Add(neighbor);
+        }
+        // Summary:
+        //      Adds newNeighbors comes as an array into Neighbors list by using AddRange.
+        // Parameters:
+        //      newNeighbors:is a Neighbor Struct array.
+        public void AddEdges(params Neighbor<T>[] newNeighbors)
+        {
+            Neighbors.AddRange(newNeighbors);
+        }
+        // Summary:
+        //      Adds newNeighbors comes as an IEnumerable list into Neighbors list by using AddRange.
+        // Parameters:
+        //      wNeighbors:is a Neighbor Struct list.
+        public void AddEdges(IEnumerable<Neighbor<T>> newNeighbors)
+        {
+            Neighbors.AddRange(newNeighbors);
+        }
+        // Summary:
+        //      Removes parameter neighbor from Neighbors.
+        // Parameters:
+        //      neighbor:is a Neighbor struct.
+        public void RemoveEdge(Neighbor<T> neighbor)
+        {
+            Neighbors.Remove(neighbor);
+        }
+
+        // Summary:
+        //          Overrided ToString method returns neighbors of neighbor in a layout."this name(F) : neighbor's names of this"
+        public override string ToString()
+        {
+            return Name+"("+F+")"+" : " +string.Join(" ", Neighbors.Select(n => n.neighbor.Name));
+        }
+
+    }
+}
---
+++ Searches/Searches.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Searches
+{
+    // Summary:
+    //      Includes search algorithms.
+    class Searches<T> 
+    {
+        public List<Node<T>> SimulatedAnnealing(PriorityQueue<Node<T>, int> openList, int temperature)
+        {
+            Stack<Node<T>> stack = new Stack<Node<T>>();
+
+            Node<T> current = openList.Dequeue();
+            Node<T> best = current;
+            PathArrange(stack, current);
+            current.IsVisited = true;
+
+            do
+            {
+                foreach (var temp in current.Neighbors)
+                {
+
+                    if (temp.neighbor.IsVisited == true)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        temp.neighbor.Depth = current.Depth + 1;
+                        openList.Enqueue(temp.neighbor);
+                    }
+
+                }
+                current.IsVisited = true;
+                Node<T> Si = openList.Dequeue();
+                if (Si.F <= current.F)
+                {
+                    current = Si;
+                    PathArrange(stack, current);
+                    current.IsVisited = true;
+                }
+                else if (Si.F <= (current.F + temperature - current.Depth))
+                {
+                    current = Si;
+                    PathArrange(stack, current);
+                    current.IsVisited = true;
+                }
+                temperature = temperature - 1;
+
+            } while (openList.Any());
+            return StackToList(stack);
+        }
+        // Summary:
+        //      Applies A Star Search algorithms using PriorityQueue Class, arranges f function and Depth of Node .
+        // Parameters:
+        //      openList:The given list it must include the initialized node.
+        //      goal:search for the goal node.
+        public List<Node<T>> AStar(PriorityQueue<Node<T>,int> openList, Node<T> goal)
+        {
+            Stack<Node<T>> stack = new Stack<Node<T>>();
+            while (openList.Any())
+            {
+                Node<T> current = openList.Dequeue();
+                PathArrange(stack,current);
+                current.IsVisited = true;
+                if (current == goal)
+                {
+                    return StackToList(stack);
+                }
+                foreach (var temp in current.Neighbors)
+                {
+                    if (temp.neighbor.IsVisited == true)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        temp.neighbor.HandG[0, 1] = current.HandG[0, 1] + temp.DistanceToNeighbor;
+                        //heuristic value is already defined at the beginning of declaration.
+                        temp.neighbor.Depth = current.Depth + 1;
+                        openList.Enqueue(temp.neighbor);
+                    }
+                }
+            }
+            return StackToList(stack);
+            
+            
+        }
+
+        private void PathArrange(Stack<Node<T>> stack, Node<T> current)
+        {
+            if (!stack.Any())
+            {
+                stack.Push(current);
+                return;
+            }
+            while (stack.Peek().Depth >= current.Depth)
+            {
+                stack.Pop();
+            }
+            stack.Push(current);
+            
+        }
+
+        private List<Node<T>> StackToList(Stack<Node<T>> stack)
+        {
+            List<Node<T>> temp = stack.ToList();
+            temp.Reverse();
+            return temp;
+        }
+        
+        // Summary:
+        //      Applies Breadth First Search algorithms using Queue Class and arranges Depth of Node.
+        // Parameters:
+        //      root:used as the beginning node.
+        //      goal:search for the goal node.
+        public List<Node<T>> BreadthFirstSearch(Node<T> root, Node<T> goal)
+        {
+            Stack<Node<T>> stack = new Stack<Node<T>>();
+            Queue<Node<T>> queue = new Queue<Node<T>>();
+            queue.Enqueue(root);
+            while (queue.Any())
+            {
+                Node<T> current = queue.Dequeue();
+                if (!current.IsVisited) {
+                    PathArrange(stack,current);
+                    current.IsVisited = true;
+
+                    if (current == goal)
+                    {
+                        return StackToList(stack);
+                    }
+                    foreach (var temp in current.Neighbors)
+                    {
+                        temp.neighbor.Depth = current.Depth + 1;
+                        queue.Enqueue(temp.neighbor);
+                    }
+                }
+            }
+            return StackToList(stack);
+        }
+    }
+}
